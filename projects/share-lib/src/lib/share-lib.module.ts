5737d635f71da06a75c54f8ffc3915c775eef1f5
Prateek Goel <pgoel681@gmail.com>
2021-05-28 17:06:37 +0530

    main

---
+++ projects/share-lib/src/lib/share-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ShareLibComponent } from './share-lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    ShareLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ShareLibComponent
+  ]
+})
+export class ShareLibModule { }
